7971511223bdc88d57d908897ce3f99794d46da2
Ramon Caballero Villegas <ramon.caballeroiv@gmail.com>
2017-08-10 18:43:13 -0600

    Implemented Threat and RangeOfMotion properties

--- Chess/Model/Ranks/Pawn.cs
+++ Chess/Model/Ranks/Pawn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chess.Control;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,35 @@ namespace Chess.Model.Ranks
 {
 	public class Pawn : Piece
 	{
-		public Pawn(int playerNumber) : base(playerNumber)
+        public override Coordinate[] Threat {
+            get {
+                int direction = (PlayerNumber == 0) ? 1 : -1;
+                return new Coordinate[] {
+                    new Coordinate((char) (CurrentPosition.Column + 1), CurrentPosition.Row + direction),
+                    new Coordinate((char) (CurrentPosition.Column - 1), CurrentPosition.Row + direction)
+                };
+            }
+        }
+
+        public override Coordinate[] RangeOfMotion {
+            get {
+                int direction = (PlayerNumber == 0) ? 1 : -1;
+                List<Coordinate> motion = new List<Coordinate> {
+                    new Coordinate(CurrentPosition.Column, CurrentPosition.Row + direction)
+                };
+                if (HasMoved) motion.Add(new Coordinate(CurrentPosition.Column, CurrentPosition.Row + (2 * direction)));
+
+                foreach (Coordinate threatMotion in Threat.Where(space => GameBoard.gameGrid[space] != null)) {
+                    motion.Add(threatMotion);
+                }
+
+                return motion.ToArray();
+            }
+        }
+
+        public Pawn(int playerNumber) : base(playerNumber)
 		{
 		}
-	}
+
+    }
 }
