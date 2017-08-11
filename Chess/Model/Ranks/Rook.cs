37989c7e2270043b8fdb8ea7ef74f43f9e2c17e7
Mooba <BConyers@student.neumont.edu>
2017-08-11 13:56:46 -0600

    Rook is donezo

--- Chess/Model/Ranks/Rook.cs
+++ Chess/Model/Ranks/Rook.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace Chess.Model.Ranks
 {
-	public class Rook : Piece
-	{
-		public Rook(int playerNumber) : base(playerNumber)
-		{
-		}
-	}
+    public class Rook : Piece
+    {
+        public Rook(int playerNumber) : base(playerNumber)
+        {
+            PlayerNumber = playerNumber;
+        }
+
+        public override Coordinate[] Threat
+        {
+            get
+            {
+                return Control.GameBoard.gameGrid.Where(space => (space.Key.Column == this.CurrentPosition.Column) || (space.Key.Row == this.CurrentPosition.Row)).Select(space => space.Key).ToArray();
+            }
+        }
+
+        public override Coordinate[] RangeOfMotion
+        {
+            get
+            {
+                return Threat;
+            }
+        }
+    }
 }
