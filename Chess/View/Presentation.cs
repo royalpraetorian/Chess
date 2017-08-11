e763bc897b350f6c5bd26c4f481b5bab76728d74
ChrisEllis <CEllis@student.neumont.edu>
2017-08-11 14:26:55 -0600

    NOW the presentation works**

--- Chess/View/Presentation.cs
+++ Chess/View/Presentation.cs
@@ -18,56 +18,63 @@ namespace Chess.View
 			bool quit = false;
 			while (!quit)
 			{
-				string userInput = Console.ReadLine().ToLower();
-				string[] commands = userInput.Split();
-				string startIndex = commands[0];
-				string endIndex = commands[2];
-				Coordinate endLocation = new Coordinate(endIndex[0], int.Parse(endIndex[1].ToString()));
-				Coordinate startLocation = new Coordinate('a', 1);
-				if (startIndex.Length > 2)
+				try
 				{
-					switch(startIndex)
+					string userInput = Console.ReadLine().ToLower();
+					string[] commands = userInput.Split();
+					string startIndex = commands[0];
+					string endIndex = commands[2];
+					Coordinate endLocation = new Coordinate(endIndex[0], int.Parse(endIndex[1].ToString()));
+					Coordinate startLocation = new Coordinate('a', 1);
+					if (startIndex.Length > 2)
 					{
-						case "pawn":
-							startLocation = GameBoard.gameGrid.Where(square => square.Value.OccupyingPiece != null && square.Value.OccupyingPiece.GetType().Equals(typeof(Pawn)) &&
-							square.Value.OccupyingPiece.RangeOfMotion.Contains(endLocation)).First().Key;
-							break;
-						case "rook":
-							startLocation = GameBoard.gameGrid.Where(square => square.Value.OccupyingPiece != null && square.Value.OccupyingPiece.GetType().Equals(typeof(Rook)) &&
-							square.Value.OccupyingPiece.RangeOfMotion.Contains(endLocation)).First().Key;
-							break;
-						case "knight":
-							startLocation = GameBoard.gameGrid.Where(square => square.Value.OccupyingPiece != null && square.Value.OccupyingPiece.GetType().Equals(typeof(Knight)) &&
-							square.Value.OccupyingPiece.RangeOfMotion.Contains(endLocation)).First().Key;
-							break;
-						case "bishop":
-							startLocation = GameBoard.gameGrid.Where(square => square.Value.OccupyingPiece != null && square.Value.OccupyingPiece.GetType().Equals(typeof(Bishop)) &&
-							square.Value.OccupyingPiece.RangeOfMotion.Contains(endLocation)).First().Key;
-							break;
-						case "king":
-							startLocation = GameBoard.gameGrid.Where(square => square.Value.OccupyingPiece != null && square.Value.OccupyingPiece.GetType().Equals(typeof(King)) &&
-							square.Value.OccupyingPiece.RangeOfMotion.Contains(endLocation)).First().Key;
-							break;
-						case "queen":
-							startLocation = GameBoard.gameGrid.Where(square => square.Value.OccupyingPiece!=null && square.Value.OccupyingPiece.GetType().Equals(typeof(Queen)) &&
-							square.Value.OccupyingPiece.RangeOfMotion.Contains(endLocation)).First().Key;
-							break;
+						switch (startIndex)
+						{
+							case "pawn":
+								startLocation = GameBoard.gameGrid.Where(square => square.Value.OccupyingPiece != null && square.Value.OccupyingPiece.GetType().Equals(typeof(Pawn)) &&
+								square.Value.OccupyingPiece.RangeOfMotion.Contains(endLocation)).First().Key;
+								break;
+							case "rook":
+								startLocation = GameBoard.gameGrid.Where(square => square.Value.OccupyingPiece != null && square.Value.OccupyingPiece.GetType().Equals(typeof(Rook)) &&
+								square.Value.OccupyingPiece.RangeOfMotion.Contains(endLocation)).First().Key;
+								break;
+							case "knight":
+								startLocation = GameBoard.gameGrid.Where(square => square.Value.OccupyingPiece != null && square.Value.OccupyingPiece.GetType().Equals(typeof(Knight)) &&
+								square.Value.OccupyingPiece.RangeOfMotion.Contains(endLocation)).First().Key;
+								break;
+							case "bishop":
+								startLocation = GameBoard.gameGrid.Where(square => square.Value.OccupyingPiece != null && square.Value.OccupyingPiece.GetType().Equals(typeof(Bishop)) &&
+								square.Value.OccupyingPiece.RangeOfMotion.Contains(endLocation)).First().Key;
+								break;
+							case "king":
+								startLocation = GameBoard.gameGrid.Where(square => square.Value.OccupyingPiece != null && square.Value.OccupyingPiece.GetType().Equals(typeof(King)) &&
+								square.Value.OccupyingPiece.RangeOfMotion.Contains(endLocation)).First().Key;
+								break;
+							case "queen":
+								startLocation = GameBoard.gameGrid.Where(square => square.Value.OccupyingPiece != null && square.Value.OccupyingPiece.GetType().Equals(typeof(Queen)) &&
+								square.Value.OccupyingPiece.RangeOfMotion.Contains(endLocation)).First().Key;
+								break;
+						}
+					}
+					else
+					{
+						startLocation = new Coordinate(startIndex[0], int.Parse(startIndex[1].ToString()));
+					}
+					Move move = new Move(startLocation, endLocation);
+					string result = GameBoard.MovePiece(move);
+					if (result == null)
+					{
+						Console.WriteLine("Move successful!");
+					}
+					else
+					{
+						Console.WriteLine("Move failed!");
+						Console.WriteLine(result);
 					}
 				}
-				else
+				catch (InvalidOperationException e)
 				{
-					startLocation = new Coordinate(startIndex[0], int.Parse(startIndex[1].ToString()));
-				}
-				Move move = new Move(startLocation, endLocation);
-				string result = GameBoard.MovePiece(move);
-				if (result==null)
-				{
-					Console.WriteLine("Move successful!");
-				}
-				else
-				{
-					Console.WriteLine("Move failed!");
-					Console.WriteLine(result);
+					Console.WriteLine("There were no pieces of the specified type that could legally move to that square.");
 				}
 			}
 		}
