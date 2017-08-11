85676c9c9a3fb7fc4779f3437d2f315dcb5a9e86
Ahrivaeus <royalpraetorian@gmail.com>
2017-08-11 01:10:56 -0600

    Queen and King Threat and Movement Options Implimented

--- Chess/Model/Ranks/King.cs
+++ Chess/Model/Ranks/King.cs
@@ -8,8 +8,26 @@ namespace Chess.Model.Ranks
 {
 	public class King : Piece
 	{
-		public King(int playerNumber) : base(playerNumber)
+		public King(int playerNumber) : base(playerNumber) { }
+
+		public override Coordinate[] Threat
 		{
+			get
+			{
+				return Control.GameBoard.gameGrid.Where(
+					square => (Math.Abs(square.Key.Column - this.CurrentPosition.Column) == 1 && Math.Abs(square.Key.Row-this.CurrentPosition.Row)==1) || 
+					(Math.Abs(square.Key.Column-this.CurrentPosition.Column)==1 && square.Key.Row == this.CurrentPosition.Row) ||
+					(Math.Abs(square.Key.Row-this.CurrentPosition.Row)==1 && square.Key.Column==this.CurrentPosition.Column)
+					).Select(square => square.Key).ToArray();
+			}
+		}
+
+		public override Coordinate[] RangeOfMotion
+		{
+			get
+			{
+				return Threat;
+			}
 		}
 	}
 }
--- Chess/Model/Ranks/Queen.cs
+++ Chess/Model/Ranks/Queen.cs
@@ -8,8 +8,25 @@ namespace Chess.Model.Ranks
 {
 	public class Queen : Piece
 	{
-		public Queen(int playerNumber) : base(playerNumber)
+		public Queen(int playerNumber) : base(playerNumber) { }
+
+		public override Coordinate[] Threat
 		{
+			get
+			{
+				return Control.GameBoard.gameGrid.Where(
+					space => (space.Key.Row-space.Key.Column == this.CurrentPosition.Row-this.CurrentPosition.Column) ||
+					(space.Key.Column==this.CurrentPosition.Column) ||
+					(space.Key.Row==this.CurrentPosition.Row)).Select(space => space.Key).ToArray();
+			}
+		}
+
+		public override Coordinate[] RangeOfMotion
+		{
+			get
+			{
+				return Threat;
+			}
 		}
 	}
 }
