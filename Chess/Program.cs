655b23a50089af3712f4fcf3e65fa88f9f4bf9e2
Unknown <BConyers@student.neumont.edu>
2017-08-18 17:16:41 -0600

    Finished presentation method

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -11,32 +11,55 @@ using System.Windows.Input;
 
 namespace Chess
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             string moveSeparate = " to ";
             string validMov;
+            Coordinate[] playerMove = new Coordinate[] { };
             do
             {
-
-                    Console.WriteLine(ConsoleCheck.BoardPrint());
-                //View.Presentation.RunPresentation();
                 do
                 {
-                    Console.WriteLine("Please enter a starting space and a destination space... (Ex: 2,4 to 4,5");
-                    string consoleIn = Console.ReadLine();
-                    string[] playermove = consoleIn.Split(new string[] { moveSeparate }, StringSplitOptions.None);
-                    string[] firstCoord = playermove[0].Split(',');
-                    string[] secondCoord = playermove[1].Split(',');
-                    Coordinate[] playerMove = { new Coordinate(int.Parse(firstCoord[0]), int.Parse(firstCoord[1])), new Coordinate(int.Parse(secondCoord[0]), int.Parse(secondCoord[1])) };
+                    string[] playerIn;
+                    bool acceptMove = false;
+                    do
+                    {
+
+                        Console.WriteLine(ConsoleCheck.BoardPrint());
+                        string consoleIn;
+                        do
+                        {
+                            Console.WriteLine("Please enter a starting space and a destination space... (Ex: 2,4 to 4,5)");
+                            consoleIn = Console.ReadLine();
+                            if (string.IsNullOrEmpty(consoleIn))
+                            {
+                                Console.WriteLine("Invalid input!");
+                            }
+                        } while (string.IsNullOrEmpty(consoleIn));
+                        playerIn = consoleIn.Split(new string[] { moveSeparate }, StringSplitOptions.None);
+                        string[] firstCoord = playerIn[0].Split(',');
+                        string[] secondCoord = playerIn[1].Split(',');
+                        acceptMove = true;
+                        try
+                        {
+                            playerMove = new Coordinate[] { new Coordinate(int.Parse(firstCoord[0]), int.Parse(firstCoord[1])), new Coordinate(int.Parse(secondCoord[0]), int.Parse(secondCoord[1])) };
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("\n-- Invalid coordinate! --\n");
+                            acceptMove = false;
+                        }
+                    } while (!acceptMove);
                     validMov = GameBoard.MovePiece(new Move(playerMove[0], playerMove[1]));
-                    if (!string.IsNullOrEmpty(validMov)){
-                        Console.WriteLine(validMov);
+                    if (!string.IsNullOrEmpty(validMov))
+                    {
+                        Console.WriteLine("\n-- " + validMov + " --\n");
                     }
                 } while (!string.IsNullOrEmpty(validMov));
 
             } while (true);
         }
-	}
+    }
 }
