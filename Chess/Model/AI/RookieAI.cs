798b494a6a8a7be0116467912431875fc1e25d9c
Ramon Caballero Villegas <ramon.caballeroiv@gmail.com>
2017-08-30 17:37:23 -0600

    Moved the Rookie AI into the main project. Incomplete functionality.

---
+++ Chess/Model/AI/RookieAI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Chess.Model;
+
+namespace ChessAI {
+
+    public class RookieAI : ComputerPlayer {
+
+        public override Move GetNextMove() {
+            Random gen = new Random();
+            
+            Move bestMove = null;
+            long bestValue = long.MinValue;
+            foreach (Piece piece in Pieces) {
+                foreach (List<Coordinate> direction in piece.ValidThreat) {
+                    foreach (Coordinate destination in direction) {
+
+                    }
+                }
+            }
+
+            return bestMove;
+        }
+    }
+}
