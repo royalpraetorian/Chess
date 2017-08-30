7e24e432f14335c0e8278253e329c4d5527b0026
Ramon Caballero Villegas <ramon.caballeroiv@gmail.com>
2017-08-30 17:35:56 -0600

    Incorporated AI classes into the main project

---
+++ Chess/Model/AI/NoobAI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Chess.Control;
+using Chess.Model;
+
+namespace ChessAI {
+
+    public class NoobAI : ComputerPlayer {
+
+        public override Move MakeMove() {
+            Random gen = new Random();
+
+            Move move = null;
+            List<Piece> ownPieces = Pieces;
+
+            do {
+                foreach (Piece curPiece in ownPieces) {
+                    bool shouldMovePiece = gen.Next(1, 101) > 60;
+                    if (!shouldMovePiece) continue;
+
+                    Coordinate start = curPiece.CurrentPosition;
+
+                    foreach (List<Coordinate> direction in curPiece.ValidThreat) {
+                        bool moveChosen = false;
+
+                        foreach (Coordinate destination in direction) {
+                            bool destinationChosen = gen.Next(2) == 0;
+
+                            if (destinationChosen) {
+                                move = new Move(start, destination);
+                                moveChosen = true;
+                                break;
+                            }
+                        }
+                        if (moveChosen) break;
+                    }
+                }
+            } while (move == null);
+
+            return move;
+        }
+    }
+}
